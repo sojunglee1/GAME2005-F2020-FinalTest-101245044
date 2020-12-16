376e053f970d412da198a9384a6fa798f5b39cb7
sojunglee1 <60055089+sojunglee1@users.noreply.github.com>
2020-12-16 16:34:55 -0600

    Allows Player to Jump onto Stairs (TASK 1 - C)

--- GAME2005_FinalTest/Assets/_Scripts/PlayerBehaviour.cs
+++ GAME2005_FinalTest/Assets/_Scripts/PlayerBehaviour.cs
@@ -40,25 +40,25 @@ public class PlayerBehaviour : MonoBehaviour
             if (Input.GetAxisRaw("Horizontal") > 0.0f)
             {
                 // move right
-                body.velocity = playerCam.transform.right * speed * Time.deltaTime;
+                body.velocity += playerCam.transform.right * speed * Time.deltaTime;
             }
 
             if (Input.GetAxisRaw("Horizontal") < 0.0f)
             {
                 // move left
-                body.velocity = -playerCam.transform.right * speed * Time.deltaTime;
+                body.velocity -= playerCam.transform.right * speed * Time.deltaTime;
             }
 
             if (Input.GetAxisRaw("Vertical") > 0.0f)
             {
                 // move forward
-                body.velocity = playerCam.transform.forward * speed * Time.deltaTime;
+                body.velocity += playerCam.transform.forward * speed * Time.deltaTime;
             }
 
             if (Input.GetAxisRaw("Vertical") < 0.0f) 
             {
                 // move Back
-                body.velocity = -playerCam.transform.forward * speed * Time.deltaTime;
+                body.velocity -= playerCam.transform.forward * speed * Time.deltaTime;
             }
 
             body.velocity = Vector3.Lerp(body.velocity, Vector3.zero, 0.9f);
@@ -67,7 +67,7 @@ public class PlayerBehaviour : MonoBehaviour
 
             if (Input.GetAxisRaw("Jump") > 0.0f)
             {
-                body.velocity = transform.up * speed * 0.1f * Time.deltaTime;
+                body.velocity += transform.up * speed * 0.1f * Time.deltaTime;
             }
 
             transform.position += body.velocity;
